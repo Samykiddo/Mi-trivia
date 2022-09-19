8c258fd5b76a38fcb684a7014e6c11f3abd0549a
Yeison Alave <109448653+Samykiddo@users.noreply.github.com>
2022-09-19 02:38:12 +0000

    readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Mi-trivia
-## Es poco pero es trabajo honesto :v
-Trivia en python parta Silabuz por Yeison Alave
+## Indicaciones para poder jugar
+La trivia consta de 6 preguntas para lograr conseguir las hipoteticas entradas de mi artista favorito.
+Cada pregunta bien respondida, desbloquea una nueva pregunta 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+RED = '\033[31m'
 GREEN = '\033[32m'
 RESET = '\033[39m'
 
@@ -9,21 +11,23 @@ print (GREEN+"Hola colega! yo se por qué estas aqui, tú tambien quieres poder 
 nombre = input("\nVaya, de tanta emocion olvide decirte mi nombre. Soy BotFrank y tú, como te llamas?: ")
 print ("\nMuy bien " + nombre + ", quiero poner a prueba tus conocimientos sobre nuestro artista favorito, las preguntas son faciles para un verdadero fan, Suerte!\n")
 #instrucciones de como jugar:
-print ("*Responde las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Enter' para enviar tu respuesta:\n Tienes", score, "puntos")
-
+print ("*Responde las siguientes preguntas escribiendo la letra de la alternativa y presionando 'Enter' para enviar tu respuesta *RECUERDA SI TE EQUIVOCAS VUELVES A EMPEZAR*:\n Tienes", score, "puntos")
 class Preguntas:
   def __init__(self, orden, respuesta):
           self.orden = orden
           self.respuesta = respuesta
-preguntas_rptas = ["¿Cuál es el verdadero nombre de Travis Scott?\na) Carlton Douglas Ridenhour \nb) Jacques Berman Webster II \nc) Marshall Mathers III \nd) Chancelor Johnathan Bennett","¿Por quién se llamó a sí mismo Travis?\na) The Scottish band \nb) His uncle \nc) The baseball player Travis Hafner \nd) Travis Barker from blink-182","¿En qué universidad estudió antes de seguir una carrera musical?\na) University of Chicago \nb) Columbia University \nc) Stanford University \nd) University of Texas San Antonio","Lanzó su tercer álbum en 2018. ¿Cómo se llamaba?\na) Megaworld \nb) Starworld \nc) Astroworld \nd) Rapworld","¿En qué juego realizó Travis Scott conciertos virtuales en vivo en 2020?\na) Minecraft \nb) Fortnite Battle Royale \nc) Apex Legend \nd) Animal Crossing: New Horizons","¿Travis tiene una hija con qué celebridad?\na) Khloé Kardashian \nb) Kourtney Kardashian \nc) Kylie Jenner \nd) Kendall Jenner"]
+preguntas_rptas = [GREEN+"¿Cuál es el verdadero nombre de Travis Scott?\na) Carlton Douglas Ridenhour \nb) Jacques Berman Webster II \nc) Marshall Mathers III \nd) Chancelor Johnathan Bennett"+RESET,GREEN+"¿Por quién se llamó a sí mismo Travis?\na) The Scottish band \nb) Su tio \nc) The baseball player Travis Hafner \nd) Travis Barker from blink-182"+RESET,GREEN+"¿En qué universidad estudió antes de seguir una carrera musical?\na) University of Chicago \nb) Columbia University \nc) Stanford University \nd) University of Texas San Antonio"+RESET,GREEN+"Lanzó su tercer álbum en 2018. ¿Cómo se llamaba?\na) Megaworld \nb) Starworld \nc) Astroworld \nd) Rapworld"+RESET,GREEN+"¿En qué juego realizó Travis Scott conciertos virtuales en vivo en 2020?\na) Minecraft \nb) Fortnite Battle Royale \nc) Apex Legend \nd) Animal Crossing: New Horizons"+RESET,GREEN+"¿Travis tiene una hija con qué celebridad?\na) Khloé Kardashian \nb) Kourtney Kardashian \nc) Kylie Jenner \nd) Kendall Jenner"+RESET]
 preguntaz = [Preguntas(preguntas_rptas[0], "b"),Preguntas(preguntas_rptas[1], "b"),Preguntas(preguntas_rptas[2], "d"),Preguntas(preguntas_rptas[3], "c"),Preguntas(preguntas_rptas[4], "b"),Preguntas(preguntas_rptas[5], "c")]
 def correr_trivia(preguntaz):
-       score = 0
+       score = random.randint(0,10)
        for pregunta in preguntaz:
             respuesta = input(pregunta.orden)
             if respuesta == pregunta.respuesta:
                  score += 1
-       print("conseguiste", score, "de", len(preguntaz))
+            else: 
+              print(RED+"Respuesta Incorrecta Intentelo Nuevamente"+RESET)
+              print(correr_trivia(preguntaz))
+       print("conseguiste", score, "de 60")
 while i_trivia == True:
   intentos += 1
   score = 0
@@ -34,7 +38,7 @@ while i_trivia == True:
   print(correr_trivia(preguntaz))
   
   score = score +20 
-  print("Excelente, has obtenido", score, "puntos")
+  print("Excelente, sumaste", score, "puntos por el intento")
 
   print("\n¿Deseas intentar la trivia nuevamente?")
   repetir_trivia = input("Ingresa 'si' para repetir, o cualquier tecla para finalizar: ").lower()
